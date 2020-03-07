9bd2b715ccdc6b1460df2bc32f8f9a1a8b00f883
Suj_an <genjicroz@gmail.com>
2020-03-07 20:54:58 +0545

    did some cleaning in server.js

--- server.js
+++ server.js
@@ -3,25 +3,26 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
 
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
+// if (process.env.NODE_ENV !== "production") {
+//   require("dotenv").config();
+// }
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express();
 
-const port = process.env.PORT || 5000;
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+const port = process.env.PORT;
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(cors());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+// if (process.env.NODE_ENV === "production") {
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "client/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
-}
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client/build", "index.html"));
+// });
+// }
 
 app.post("/payment", (req, res) => {
   const body = {
